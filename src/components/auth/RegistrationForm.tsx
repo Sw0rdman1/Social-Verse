ab590caa2f8cdfef7e7988aae065da73a0f01ade
Božidar <vujasinovicb2019@gmail.com>
2023-12-12 00:26:48 +0100

    Add AuthText component to RegistrationForm

--- src/components/auth/RegistrationForm.tsx
+++ src/components/auth/RegistrationForm.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { StackScreenProps } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import BackButton from "./BackButton";
+import AuthText from "./AuthText";
 
 interface Props {
   navigation: StackScreenProps<any>["navigation"];
@@ -18,6 +19,7 @@ const RegistrationForm = ({ navigation, setButtonDisabled }: Props) => {
     <View style={styles.container}>
       <StatusBar style="dark" />
       <BackButton handleBackButton={handleBackButton} />
+      <AuthText subtitle="Create an account!" />
       <Text>RegistrationForm</Text>
     </View>
   );
